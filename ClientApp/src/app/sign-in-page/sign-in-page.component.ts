33286bf8d727cd37871963145b802a64500a20ba
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-02-28 22:35:50 +0100

    Created sign in page

--- ClientApp/src/app/sign-in-page/sign-in-page.component.ts
+++ ClientApp/src/app/sign-in-page/sign-in-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.css']
+  styleUrls: ['./sign-in-page.component.css'],
 })
 export class SignInPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
