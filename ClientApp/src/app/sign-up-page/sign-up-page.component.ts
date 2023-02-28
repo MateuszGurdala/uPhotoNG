38eeed17ab7302b67ab5c5f25bb7cfe1e64483dc
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-02-28 20:41:57 +0100

    Added basic data verification with reactive UI

--- ClientApp/src/app/sign-up-page/sign-up-page.component.ts
+++ ClientApp/src/app/sign-up-page/sign-up-page.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import ToolBox from '../scripts/tool-box.service';
 
-type RegisterData = {
+interface RegisterData {
   login: string;
   email: string;
   password: string;
   confirmation: string;
-};
+}
+
+interface Conditions {
+  allFilled: boolean;
+  loginFilled: boolean;
+  emailFilled: boolean;
+  passwordFilled: boolean;
+  confirmationFilled: boolean;
+  loginValid: boolean;
+  emailValid: boolean;
+  passwordValid: boolean;
+  containsNumber: boolean;
+  containsUppercase: boolean;
+  lengthValid: boolean;
+  areSame: boolean;
+}
 
 @Component({
   selector: 'app-sign-up-page',
@@ -17,17 +34,85 @@ export class SignUpPageComponent implements OnInit {
     login: '',
     email: '',
     password: '',
-    confirmation: ''
+    confirmation: '',
   };
-
+  cond: Conditions = {} as Conditions;
   isProcessing: boolean = false;
 
-  constructor() {}
+  constructor(private toolbox: ToolBox, private toastr: ToastrService) {
+    this.resetConditions();
+  }
 
   ngOnInit(): void {}
 
+  resetConditions() {
+    this.cond = {
+      allFilled: true,
+      loginFilled: true,
+      emailFilled: true,
+      passwordFilled: true,
+      confirmationFilled: true,
+      loginValid: true,
+      emailValid: true,
+      passwordValid: true,
+      containsNumber: true,
+      containsUppercase: true,
+      lengthValid: true,
+      areSame: true,
+    };
+  }
+
   onSubmit() {
-    this.isProcessing = true;
-    console.log(this.registerData);
+    this.resetConditions();
+
+    //Removing white spaces
+    for (const [key, value] of Object.entries(this.registerData)) {
+      this.registerData[key as keyof RegisterData] =
+        this.toolbox.removeWhiteSpaces(value);
+    }
+
+    //No empty inputs
+    for (const [key, value] of Object.entries(this.registerData)) {
+      if (this.toolbox.ifStrEmpty(value)) {
+        this.cond[(key + 'Filled') as keyof Conditions] = false;
+        this.cond.allFilled = false;
+      }
+    }
+
+    //Verify login and email are unused
+    //TODO
+
+    //Password verification
+    //Length at least 8
+    if (!this.toolbox.strLongerThan(this.registerData.password, 7)) {
+      this.cond.passwordValid = false;
+      this.cond.lengthValid = false;
+      this.cond.passwordFilled = false;
+      this.cond.confirmationFilled = false;
+    }
+
+    //Contains number
+    if (!this.toolbox.strContainsNum(this.registerData.password)) {
+      this.cond.passwordValid = false;
+      this.cond.containsNumber = false;
+      this.cond.passwordFilled = false;
+      this.cond.confirmationFilled = false;
+    }
+
+    //Contains uppercase
+    if (!this.toolbox.strContainsUppercase(this.registerData.password)) {
+      this.cond.passwordValid = false;
+      this.cond.containsUppercase = false;
+      this.cond.passwordFilled = false;
+      this.cond.confirmationFilled = false;
+    }
+
+    //Passwords are same
+    if (!(this.registerData.password === this.registerData.confirmation)) {
+      this.cond.passwordValid = false;
+      this.cond.areSame = false;
+      this.cond.passwordFilled = false;
+      this.cond.confirmationFilled = false;
+    }
   }
 }
