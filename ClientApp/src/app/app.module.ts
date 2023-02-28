2bc67f5cb6af605201b8d34f63e59e84bbeda1c5
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-02-28 22:35:05 +0100

    Added Ionic framework to project

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,16 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { CustomComponentsModule } from './custom-components/custom-components.module';
 import ToolBox from './scripts/tool-box.service';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
@@ -27,16 +26,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    IonicModule,
+    CustomComponentsModule,
+    FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: 'SignIn', component: SignInPageComponent },
       { path: 'SignUp', component: SignUpPageComponent },
       { path: '', component: WelcomePageComponent, pathMatch: 'full' },
       { path: '**', redirectTo: '/' },
     ]),
-    CustomComponentsModule,
-    FormsModule,
-    BrowserAnimationsModule,
     ToastrModule.forRoot({
       "closeButton": false,
       "newestOnTop": true,
@@ -46,7 +44,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       "timeOut": 50000,
       "tapToDismiss": true,
       "toastClass": "custom-toast ngx-toastr"
-    })
+    }),
+    IonicModule.forRoot()
   ],
   providers: [ToolBox],
   bootstrap: [AppComponent],
