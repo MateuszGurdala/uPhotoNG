f7ca107ead56c63409353fab029d41b32812ea4c
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-02 21:54:56 +0100

    Added custom controller base class

---
+++ uPhotoNG/Controllers/CustomControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using uPhotoNG.Database;
+
+namespace uPhotoNG.Controllers
+{
+    public class CustomControllerBase : Controller
+    {
+        private UnitOfWork _unitOfWork;
+
+        public CustomControllerBase(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public dynamic GetRequestBody()
+        {
+            StreamReader reader = new StreamReader(Request.Body);
+            var text = reader.ReadToEndAsync().Result;
+            return JObject.Parse(text);
+        }
+    }
+}
