7e6f6c2e41c26424bb379084d53e62bca55c1aa3
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-03 10:40:59 +0100

    Added custom header component

--- uPhotoNG/ClientApp/src/app/custom-components/custom-components.module.ts
+++ uPhotoNG/ClientApp/src/app/custom-components/custom-components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { UInputComponent } from './uinput/uinput.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent, UInputComponent],
+  declarations: [FooterComponent, UInputComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [FooterComponent, UInputComponent],
+  exports: [FooterComponent, UInputComponent, HeaderComponent],
 })
 export class CustomComponentsModule {}
