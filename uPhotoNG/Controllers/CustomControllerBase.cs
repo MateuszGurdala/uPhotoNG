13bb16a666642dd5744d57a0c664c7cf30ab9a4e
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-12 17:31:41 +0100

    Added method to get User Id from cookie

--- uPhotoNG/Controllers/CustomControllerBase.cs
+++ uPhotoNG/Controllers/CustomControllerBase.cs
@@ -24,10 +24,28 @@ namespace uPhotoNG.Controllers
         internal async Task CheckIfAuthenticated()
         {
             var result = await HttpContext.AuthenticateAsync();
-            if (!result.Succeeded) 
+            if (!result.Succeeded)
             {
                 throw new Exception("User is not authenticated");
             }
+        }
+
+        internal string GetSessionUserId()
+        {
+            var authFeatures = HttpContext.Features.Get<IAuthenticateResultFeature>();
+            if (authFeatures != null && authFeatures.AuthenticateResult != null)
+            {
+                var prop = authFeatures.AuthenticateResult.Properties;
+                if (prop != null && prop.Items != null)
+                {
+                    var id = prop.Items["Id"];
+                    if (id != null)
+                    {
+                        return id;
+                    }
+                }
+            }
+            throw new Exception("Id is null");
         }
     }
 }
