90f6a3e642df698c2c71290e246e1811c37a1c94
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-12 17:41:09 +0100

    Fixed modal bug

--- uPhotoNG/ClientApp/src/app/custom-components/app-modal/app-modal.component.ts
+++ uPhotoNG/ClientApp/src/app/custom-components/app-modal/app-modal.component.ts
@@ -1,27 +1,35 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import ModalService from '../../services/modal-service.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './app-modal.component.html',
-  styleUrls: ['./app-modal.component.css']
+  styleUrls: ['./app-modal.component.css'],
 })
 export class AppModalComponent implements OnInit {
-
   isExtended: boolean = false;
 
   @ViewChild('self') self: ElementRef;
   @HostListener('document:click', ['$event'])
-  clicked(event: Event)
-  {
-    if(!this.self.nativeElement.contains(event.target) && (event.target as HTMLElement).ariaLabel !== "apps outline")
-    {
+  clicked(event: Event) {
+    console.log();
+    let isTargetModal: boolean = this.self.nativeElement.contains(event.target);
+    let isTargetAppIcon: boolean =
+      (event.target as HTMLElement).ariaLabel === 'apps outline';
+    let isTargetAppText: boolean =
+      (event.target as HTMLElement).innerText === 'All apps';
+    if (!isTargetModal && !isTargetAppIcon && !isTargetAppText) {
       this.isExtended = false;
     }
   }
 
-  constructor(private modal: ModalService) { }
+  constructor(private modal: ModalService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
