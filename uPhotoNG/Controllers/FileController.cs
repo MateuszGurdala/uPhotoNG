20d0003c1637b5af6d4f5e5fb9c531c36f6e82e1
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-14 14:44:14 +0100

    Added template for file controller

---
+++ uPhotoNG/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uPhotoNG.Database;
+
+namespace uPhotoNG.Controllers
+{
+    public class FileController : CustomControllerBase
+    {
+        public FileController(UnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        //[HttpPost]
+        //public async Task<IActionResult> UploadPhoto()
+        //{
+        //    try
+        //    {
+        //        await CheckIfAuthenticatedAsync();
+        //        var userId = GetSessionUserId();
+
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return StatusCode(400);
+        //    }
+        //}
+    }
+}
