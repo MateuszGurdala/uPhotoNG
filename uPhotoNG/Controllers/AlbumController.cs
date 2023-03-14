9efb03c64a1592c621bcf2cfa8279656778e093f
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-03-14 12:24:10 +0100

    Added endpoints to retrieve user albums and places

---
+++ uPhotoNG/Controllers/AlbumController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uPhotoNG.Database;
+using uPhotoNG.Models.Interfaces;
+
+namespace uPhotoNG.Controllers
+{
+    public class AlbumController : CustomControllerBase
+    {
+        public AlbumController(UnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserAlbums()
+        {
+            try
+            {
+                var albums = await GetUserAlbumsAsync() as IEnumerable<IDatabaseOption>;
+                return Json(albums.Select(e => e.GetAsDbOption()).ToList());
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+    }
+}
---
+++ uPhotoNG/Controllers/PlaceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uPhotoNG.Database;
+using uPhotoNG.Models.Interfaces;
+
+namespace uPhotoNG.Controllers
+{
+    public class PlaceController : CustomControllerBase
+    {
+        public PlaceController(UnitOfWork unitOfWork) : base(unitOfWork){}
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserPlaces()
+        {
+            try
+            {
+                var places = await GetUserPlacesAsync() as IEnumerable<IDatabaseOption>;
+                return Json(places.Select(e => e.GetAsDbOption()).ToList());
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+    }
+}
